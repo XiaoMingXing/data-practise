c6b231f33db78aad5c9d3e2f63afc9ce89d2acc6
sgmxxiao <xiaomixin@gmail.com>
2018-08-19 16:56:45 +0800

    [Mingxing] add schema registry support

--- data-lambda/src/main/java/com/realtime/common/Constants.java
+++ data-lambda/src/main/java/com/realtime/common/Constants.java
@@ -4,5 +4,5 @@ public class Constants {
 
     public static final String KAFKA_BROKERS = "18.136.30.81:9092,52.221.68.56:9092";
     public static final String KAFKA_TOPICS = "click-stream";
-    public static final String SCHEMA_REGISTRY_URL = "";
+    public static final String SCHEMA_REGISTRY_URL = "http://13.229.105.134:8081";
 }
